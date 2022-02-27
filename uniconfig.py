73ae398c07a038094b22fe50adc6f52f37f4402e
Yuki Kurosawa <yuki@gakkougurashi.net>
2022-02-27 21:38:32 +0800

    Add ETH Main WETH

--- uniconfig.py
+++ uniconfig.py
@@ -22,6 +22,7 @@ class Web3UniswapConfig:
     @property
     def UNISWAP_WETH(self)->str:
         return "0xc778417E063141139Fce010982780140Aa0cD5Ab"
+        #0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2
 
     @property
     def NULLPTR(self)->str:
